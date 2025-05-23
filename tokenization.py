6a38393462d62cefeb14b021b489ca7e3899c9b3
Arnav Bharti <arnavbharti@icloud.com>
2025-05-23 17:29:13 +0530

    create datasets and dataloaders

--- tokenization.py
+++ tokenization.py
@@ -1,7 +1,7 @@
 import os
-import string
 import urllib.request
-import re
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 import tiktoken
 
@@ -50,6 +50,47 @@ import tiktoken
 #     return vocab
 
 
+class GPT2Dataset(Dataset):
+    def __init__(self, text, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(text)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i : i + max_length]
+            target_chunk = token_ids[i + 1 : i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader(
+    text,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPT2Dataset(text, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+    return dataloader
+
+
 def main():
 
     # Download the text file from the internet if it doesn't exist
@@ -93,6 +134,7 @@ def main():
     # print(len(ids))
     # print(tokenizer.decode(ids[:100]))
 
+    dataloader = create_dataloader(raw_text,batch_size=8,max_length=4,stride=4,shuffle=False,)
 
 
 if __name__ == "__main__":
